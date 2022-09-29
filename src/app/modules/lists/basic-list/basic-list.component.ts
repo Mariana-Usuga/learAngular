02eb245349fb0302aa95ceaae088623cafcd27a8
Mariana Usuga <musuga@itteamcorp.com>
2022-09-29 17:58:23 -0500

    create module

--- src/app/modules/lists/basic-list/basic-list.component.ts
+++ src/app/modules/lists/basic-list/basic-list.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { timeout } from 'rxjs';
+
+export type Product = {
+  name: string;
+  price: number;
+  description: string
+}
 
 @Component({
   selector: 'app-basic-list',
@@ -7,9 +14,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicListComponent implements OnInit {
 
+  loading: boolean = false;
+  listElements: Product[] = [
+    {
+      name: "leche",
+      price: 123,
+      description: 'descr'
+    },
+    {
+      name: "huevos",
+      price: 123,
+      description: 'descr'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  changeLoading(){
+    this.loading = !this.loading;
   }
 
 }
